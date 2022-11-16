27e38a9dcc2c6a19b087609b61cfe8f775830c74
imokoi <tiantengfei@outlook.com>
2022-11-17 01:06:18 +0900

    add event

--- examples/chatting/src/main.rs
+++ examples/chatting/src/main.rs
@@ -1,5 +1,3 @@
 fn main() {
     println!("Hello, world!");
-    let num = gamer::add(2, 2);
-    println!("num = {}", num);
 }
--- gamer/src/lib.rs
+++ gamer/src/lib.rs
@@ -1,18 +1,62 @@
-pub fn hello() {
-    println!("Hello, libs!");
+use serde::Serialize;
+use std::{collections::HashMap, fmt::Debug};
+
+// Store is used to store the states of the game
+// it is a singleton and can be accessed from anywhere
+pub struct Store<S> {
+    states: HashMap<String, S>,
 }
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub trait MessageData: Debug {}
+
+// websocket messages
+#[derive(Debug)]
+pub struct WebsocketMessage {
+    pub code: usize,
+    pub message: Box<dyn MessageData>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+type MessageCode = usize;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub struct Event {
+    pub code: MessageCode,
+    pub handler: Box<dyn Fn(WebsocketMessage)>,
+}
+
+pub trait EventObserver {
+    fn on_event(&mut self, code: MessageCode, handler: Box<dyn Fn(WebsocketMessage)>);
+}
+
+pub trait EventRunner {
+    fn run(&mut self, code: MessageCode, message: WebsocketMessage);
+}
+
+pub struct Gamer {
+    pub events: HashMap<MessageCode, Event>,
+}
+
+impl Gamer {
+    pub fn new() -> Self {
+        Gamer {
+            events: HashMap::new(),
+        }
+    }
+}
+
+impl EventObserver for Gamer {
+    fn on_event(&mut self, code: MessageCode, handler: Box<dyn Fn(WebsocketMessage)>) {
+        if let Some(event) = self.events.get(&code) {
+            panic!("Event with code {} already exists", event.code);
+        } else {
+            self.events.insert(code, Event { code, handler });
+        }
+    }
+}
+
+impl EventRunner for Gamer {
+    fn run(&mut self, code: MessageCode, message: WebsocketMessage) {
+        if let Some(event) = self.events.get(&code) {
+            (event.handler)(message);
+        }
     }
 }
